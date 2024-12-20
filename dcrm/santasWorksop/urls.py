2ecd81586bb1357138e8ce33f27886fdfedbe6c7
collegeGent <rgent@rnngroup.ac.uk>
2024-12-20 11:34:08 +0000

    added navbar

--- dcrm/santasWorksop/urls.py
+++ dcrm/santasWorksop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name=""),
+    path("shop", views.shop, name="shop"),
 ]
--- dcrm/santasWorksop/views.py
+++ dcrm/santasWorksop/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
+
+def shop(request):
+    return render(request,'pages/shop.html')
